fe921eebf255e6e40a5426f19490b0dcf8e73391
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-02-24 18:15:31 +0500

    awdawd

--- src/Providers/OctoServiceProvider.php
+++ src/Providers/OctoServiceProvider.php
@@ -13,9 +13,9 @@ class OctoServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->mergeConfigFrom(__DIR__ . '../../config/config.php', 'octo');
-        $this->loadRoutesFrom(__DIR__ . '../../routes/web.php');
-        $this->loadMigrationsFrom(__DIR__ . '../../database/migrations');
+        $this->mergeConfigFrom(__DIR__ . '/..config/config.php', 'octo');
+        $this->loadRoutesFrom(__DIR__ . '/../routes/web.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 //        $this->loadViewsFrom(__DIR__.'/views', 'todolist');
 //        $this->publishes([
 //            __DIR__.'/views' => base_path('resources/views/wisdmlabs/todolist'),
