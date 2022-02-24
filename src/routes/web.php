abe5239c5c8851803de317f61c6de2fd4ec34235
Asadbekinha <fayzulloevasadbek@gmail.com>
2022-02-24 17:56:58 +0500

    new feature

--- src/routes/web.php
+++ src/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Asadbek\OctoLaravel\Http\Controllers\OctoBaseController;
+Route::group([
+    'middleware' => 'web',
+    'prefix' => 'octo',
+    'as' => 'octo.',
+], function(){
+    Route::get('/pay/{order}', [OctoBaseController::class, 'pay'])->name("pay");
+    Route::get('/verify/{order}', [OctoBaseController::class, 'verify'])->name("verify");
+    Route::get('/notify/{order}', [OctoBaseController::class, 'notify'])->name("notify");
 
-Route::get('/pay/{order}', [OctoBaseController::class, 'pay'])->name("octo.pay");
-Route::get('/verify/{order}', [OctoBaseController::class, 'verify'])->name("octo.verify");
-Route::get('/notify/{order}', [OctoBaseController::class, 'notify'])->name("octo.notify");
+});
